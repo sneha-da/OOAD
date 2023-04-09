a2196883c68bd8ef7bb54e3740b6ed8114ebc1b6
sneha-da <91374614+sneha-da@users.noreply.github.com>
2023-04-09 20:59:02 +0530

    Update test_case_generation.py

--- test_case_generation.py
+++ test_case_generation.py
@@ -1,7 +1,6 @@
 import openai
 import streamlit as st
 
-openai.api_key = "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"
 
 def gpt3_chatbot(prompt):
     response = openai.Completion.create(
@@ -13,6 +12,11 @@ def gpt3_chatbot(prompt):
         temperature=0.5,
     )
     return response.choices[0].text.strip()
+
+with st.sidebar:
+    key_input = st.text_input('Enter your secret Key')
+
+openai.api_key = key_input
 
 st.title("AI for Generating Test Cases")
 st.write("This is a AI developed by Sneha D A, Bhava, Maharnav Sahu of VIT which generates test cases of every use case you ask, can also be used to clarify educational doubts with it.")
